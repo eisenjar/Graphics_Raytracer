ba7238516ecfd51056f504c095780c9cdbe8e826
Eisen Jared <c4eisenj@cdf.toronto.edu>
2014-11-20 16:43:50 -0500

    First commit

---
+++ scene_object.cpp
@@ -0,0 +1,61 @@
+/***********************************************************
+     Starter code for Assignment 3
+
+     This code was originally written by Jack Wang for
+		    CSC418, SPRING 2005
+
+		implements scene_object.h
+
+***********************************************************/
+
+#include <cmath>
+#include <iostream>
+#include "scene_object.h"
+
+bool UnitSquare::intersect( Ray3D& ray, const Matrix4x4& worldToModel,
+		const Matrix4x4& modelToWorld ) {
+	// TODO: implement intersection code for UnitSquare, which is
+	// defined on the xy-plane, with vertices (0.5, 0.5, 0), 
+	// (-0.5, 0.5, 0), (-0.5, -0.5, 0), (0.5, -0.5, 0), and normal
+	// (0, 0, 1).
+	//
+	// Your goal here is to fill ray.intersection with correct values
+	// should an intersection occur.  This includes intersection.point, 
+	// intersection.normal, intersection.none, intersection.t_value.   
+	//
+	// HINT: Remember to first transform the ray into object space  
+	// to simplify the intersection test.
+	//_modelToWorld = _modelToWorld*node->trans;
+	//_worldToModel = node->invtrans*_worldToModel;
+	
+	//backface cull; modify this if necessary
+	if(ray.origin.m_data[2] * ray.dir.m_data[2] >= 0) return false;
+
+	double scale_factor = ray.origin.m_data[2]/ray.dir.m_data[2];
+	Vector3D scaled_dir = scale_factor*ray.dir;
+	Point3D POI = ray.origin + scaled_dir;
+
+	ray.intersection.point = POI;
+
+	//_worldToModel = node->trans*_worldToModel;
+	//_modelToWorld = _modelToWorld*node->invtrans;
+	
+
+	return false;
+}
+
+bool UnitSphere::intersect( Ray3D& ray, const Matrix4x4& worldToModel,
+		const Matrix4x4& modelToWorld ) {
+	// TODO: implement intersection code for UnitSphere, which is centred 
+	// on the origin.  
+	//
+	// Your goal here is to fill ray.intersection with correct values
+	// should an intersection occur.  This includes intersection.point, 
+	// intersection.normal, intersection.none, intersection.t_value.   
+	//
+	// HINT: Remember to first transform the ray into object space  
+	// to simplify the intersection test.
+	
+	return false;
+}
+
