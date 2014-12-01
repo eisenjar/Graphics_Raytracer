88e37afa5be613b3aeb766b856b47e18083e132a
eisenjar <c4eisenj@cdf.toronto.edu>
2014-11-30 20:44:53 -0500

    added diffuse

--- light_source.cpp
+++ light_source.cpp
@@ -12,17 +12,21 @@
 #include "light_source.h"
 
 #define AMB 1
-#define DIFF 0
-#define SPEC 0
+#define DIFF 1
+#define SPEC 1
 
 void PointLight::shade( Ray3D& ray ) {
+
+	Vector3D light_to_intersection = ray.intersection.point - this->_pos;
+	double intersection_dot_normal = -light_to_intersection.dot(ray.intersection.normal);
 
 	#if AMB
 	if(!ray.amb) ray.col = ray.intersection.mat->ambient;
 	#endif
 
 	#if DIFF
-
+	
+	if(intersection_dot_normal > 0) ray.col = ray.col + ray.intersection.mat->diffuse;
 	#endif
 
 	// TODO: implement this function to fill in values for ray.col 
